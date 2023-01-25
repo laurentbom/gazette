5cd3312c507d74dc25d983daeb03eeecbbdf5f05
LaurentB <laurent.bomal@gmail.com>
2023-01-25 07:23:09 +0100

    javascript-done

--- js/main.js
+++ js/main.js
@@ -1,11 +1,41 @@
+// Barre de recherche
+var barreNav = document.querySelector(".header__main_nav__liens");
+var barreRecherche = document.querySelector(".searchbar");
+var iconeLoupe = document.querySelector(".header__main_nav__search");
+
+iconeLoupe.addEventListener('click',function(){
+  barreNav.classList.toggle("hide");
+  barreRecherche.classList.toggle("visible");
+});
+
+// Icone to the top
+var iconeTop = document.querySelector(".toTheTop");
+var scrollAffichage = window.innerHeight / 10;
+var lastScroll = 0;
+window.addEventListener("scroll", function(){
+  var scrolled = window.scrollY;
+  if(scrolled >= scrollAffichage) {
+    iconeTop.classList.add('show');
+  } else {
+    iconeTop.classList.remove('show');
+  }
+  lastScroll = scrolled;
+});
+
+// Swipers.js
 const swiper = new Swiper('.swiper1', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
   slidesPerView: 3,
   grabCursor: true,
-  speed: 2000,
+  speed: 1000,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
 });
+// obliger de concaténé deux classes pour avoir plusieurs swiper .swiper .two
 const swiper2 = new Swiper('.swiper.two', {
   // Optional parameters
   direction: 'horizontal',
@@ -17,3 +47,4 @@ const swiper2 = new Swiper('.swiper.two', {
   },
   speed: 1000,
 });
+
